c479549d0e456dab7ade91214b09c0dfcfe2aade
KingaStefanska <kinga0stefanska@gmail.com>
2023-01-17 09:26:58 +0100

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,40 +1,35 @@
 import React from 'react';
-import { Component } from 'react';
+import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import css from './Modal.module.css';
 
-class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyPress);
-  }
+const Modal = ({ modalImage, tags, closeModal }) => {
+  useEffect(() => {
+    const handleKeyPress = event => {
+      if (event.code === 'Escape') {
+        closeModal();
+      }
+    };
+    window.addEventListener('keydown', handleKeyPress);
+    return () => {
+      window.removeEventListener('keydown', handleKeyPress);
+    };
+  }, [closeModal]);
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyPress);
-  }
-
-  handleKeyPress = key => {
-    if (key.code === 'Escape') {
-      this.props.closeModal();
-    }
-  };
-
-  backdropOnClick = event => {
+  const backdropOnClick = event => {
     if (event.currentTarget === event.target) {
-      this.props.closeModal();
+      closeModal();
     }
   };
 
-  render() {
-    const { modalImage, tags } = this.props;
-    return (
-      <div className={css.Overlay} onClick={this.backdropOnClick}>
-        <div className={css.Modal}>
-          <img src={modalImage} alt={tags} />
-        </div>
+  return (
+    <div className={css.Overlay} onClick={backdropOnClick}>
+      <div className={css.Modal}>
+        <img src={modalImage} alt={tags} />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 Modal.propTypes = {
   handleKeyPress: PropTypes.func.isRequired,
